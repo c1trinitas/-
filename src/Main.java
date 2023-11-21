3ee87b35633ddfb6f40ac3c1537e992df736c8c1
citrinitas <daniargizatullin8@gmail.com>
2023-11-21 09:53:26 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,83 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println ("Задача 1");
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println ("Задача 2.");
+        System.out.println (dog= dog +4);
+        System.out.println (cat = cat + 4);
+        System.out.println (paper = paper + 4);
+        System.out.println ("Задача 3.");
+        dog = (int) (dog - 3.5);
+        cat = (int) (cat - 1.6);
+        paper = (paper - 7639);
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        System.out.println ("Задача 4.");
+        var friend = 19;
+        System.out.println(friend);
+        System.out.println(friend = friend + 2);
+        var friendGroup1 = 19;
+        var friendGroup2 = 2;
+        int friends;
+        System.out.println(friends = friendGroup1 + friendGroup2);
+        int teams;
+        System.out.println(teams = friends / 7);
+        System.out.println("Задача 5");
+        var frog = 3.5;
+        var frogs = 3.5 * 10;
+        var notFrogs = frogs / 10;
+        System.out.println(frog);
+        System.out.println(frogs);
+        System.out.println(notFrogs);
+        var swamp = notFrogs + 4;
+        System.out.println(swamp);
+        System.out.println("Задача 6");
+        var fighter1 = 78.2;
+        var fighter2 = 82.7;
+        var fightersWeight = fighter1 + fighter2;
+        System.out.println("Общая масса" + fightersWeight + "кг");
+        var fightersDifference = fighter2 - fighter1;
+        System.out.println("Разница в весе" + fightersDifference + "кг");
+        System.out.println("Задача 7");
+        var fightersD =   fighter2 - fighter1;
+        System.out.println("Разница в весе" + fightersD + "кг");
+        var fightersDifference2 = fighter2 % fighter1;
+        System.out.println("Разница в весе в процентном соотношении" + fightersDifference2 + "процентов");
+        System.out.println("Задача 8");
+        var totalTime = 640;
+        var workTime = 8;
+        var staff = totalTime / workTime;
+        System.out.println("Всего сотрудников компании" + staff + "человек");
+        var totalStaff = staff + 94;
+        System.out.println(totalStaff);
+        var newWorktime = totalTime / totalStaff;
+        System.out.println("Если в компании работает" + totalStaff + ", то всего" + newWorktime +"часов работы");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
